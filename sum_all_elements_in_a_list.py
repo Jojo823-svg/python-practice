818554238e3a749e1d5032f727a998e23a3ec1ad
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-04-26 02:36:17 +0300

    Sum of the elements in a list

---
+++ sum_all_elements_in_a_list.py
@@ -0,0 +1,9 @@
+#Definition of the function
+def sum_number(num):
+ sum=0                #At this point the sum should be initialized to 0 for accurate addition
+ for number in num :   #The for loop enables iteration that will help perform addition
+   sum+=number           #This enables us to add the numbers to the current sum
+ return sum  #it returns the sum of the elements
+
+num=[1,2,3,4,5,6,7,8,9]
+print(sum_number(num))
